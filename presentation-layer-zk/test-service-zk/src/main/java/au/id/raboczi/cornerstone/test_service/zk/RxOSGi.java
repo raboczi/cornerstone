6a5aa5459f3eab56544f920042021699e11c433d
raboczi <raboczi@icloud.com>
2020-04-28 20:50:20 +1000

    Created RxOSGi adapter from OSGi event admin to RxJava.
    Used this new adapter to improve TestController.

---
+++ presentation-layer-zk/test-service-zk/src/main/java/au/id/raboczi/cornerstone/test_service/zk/RxOSGi.java
@@ -0,0 +1,98 @@
+package au.id.raboczi.cornerstone.test_service.zk;
+
+/*-
+ * #%L
+ * Cornerstone :: Test service ZK UI
+ * %%
+ * Copyright (C) 2019 - 2020 Simon Raboczi
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import io.reactivex.Observable;
+import java.util.Hashtable;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.osgi.service.event.Event;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import static org.osgi.service.event.EventConstants.EVENT_TOPIC;
+import org.osgi.service.event.EventHandler;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Integrations between RxJava and OSGi.
+ */
+public abstract class RxOSGi {
+
+    /** Logger.  Named after the class. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RxOSGi.class);
+
+    /**
+     * @param topic  an OSGi event admin topic
+     * @param bundleContext  the OSGi bundle context upon which the event handler will be registered
+     * @return an RxJava Observable
+     */
+    public static Observable<Event> fromTopic(final String topic, final BundleContext bundleContext) {
+        return Observable.fromPublisher(publisherFromTopic(topic, bundleContext));
+    }
+
+    private static Publisher<Event> publisherFromTopic(final String topic, final BundleContext bundleContext) {
+        return new Publisher<Event>() {
+            @Override
+            public void subscribe(final Subscriber<? super Event> subscriber) {
+                subscriber.onSubscribe(new Subscription() {
+
+                    private @Nullable ServiceRegistration registration = null;
+
+                    @Override
+                    public void cancel() {
+                        LOGGER.info("Cancel subscription");
+                        if (registration != null) {
+                            registration.unregister();
+                            registration = null;
+                        }
+                    }
+
+                    @Override
+                    public void request(final long dummy) {
+                        LOGGER.info("Request subscription, dummy={}", dummy);
+
+                        EventHandler eventHandler = new EventHandler() {
+                            @Override
+                            public void handleEvent(final Event event) {
+                                try {
+                                    subscriber.onNext(event);
+
+                                } catch (Exception e) {
+                                    subscriber.onError(e);
+                                }
+                            }
+                        };
+
+                        Hashtable ht = new Hashtable();
+                        ht.put(EVENT_TOPIC, new String[] {topic});
+
+                        registration = bundleContext.registerService(EventHandler.class.getName(), eventHandler, ht);
+                    }
+                });
+            }
+        };
+    }
+}
--- presentation-layer-zk/test-service-zk/src/main/java/au/id/raboczi/cornerstone/test_service/zk/TestController.java
+++ presentation-layer-zk/test-service-zk/src/main/java/au/id/raboczi/cornerstone/test_service/zk/TestController.java
@@ -25,28 +25,19 @@ package au.id.raboczi.cornerstone.test_service.zk;
 import au.id.raboczi.cornerstone.Caller;
 import au.id.raboczi.cornerstone.CallerNotAuthorizedException;
 import au.id.raboczi.cornerstone.test_service.TestService;
-import au.id.raboczi.cornerstone.zk.CallerImpl;
 import au.id.raboczi.cornerstone.zk.Users;
 import au.id.raboczi.cornerstone.zk.util.Reference;
 import au.id.raboczi.cornerstone.zk.util.SCRSelectorComposer;
-import java.util.Hashtable;
 import java.util.concurrent.Callable;
-import java.util.function.Consumer;
 import org.checkerframework.checker.i18n.qual.Localized;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.osgi.service.event.Event;
-import static org.osgi.service.event.EventConstants.EVENT_TOPIC;
-import org.osgi.service.event.EventHandler;
-import org.osgi.service.useradmin.User;
 import org.osgi.service.useradmin.UserAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Sessions;
-//import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.event.EventListener;
-import org.zkoss.zk.ui.event.EventQueue;
-import org.zkoss.zk.ui.event.EventQueues;
+import org.zkoss.zk.ui.Desktop;
+import org.zkoss.zk.ui.DesktopUnavailableException;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.MouseEvent;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
@@ -56,14 +47,10 @@ import org.zkoss.zul.Label;
 /**
  * Controller for the macro <code>test.zul</code>.
  */
-public final class TestController extends SCRSelectorComposer<Component>
-    implements EventListener<ConsumerEvent<String>> {
+public final class TestController extends SCRSelectorComposer<Component> {
 
     /** Logger.  Named after the class. */
     private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
-
-    /** The name of the ZK queue. */
-    private static final String QUEUE = "test-consumer-queue-name";
 
     /** The model for this controller. */
     @Reference
@@ -76,6 +63,14 @@ public final class TestController extends SCRSelectorComposer<Component>
     /** The view for this controller. */
     @Wire("#label1")
     private @Nullable Label label1;
+
+    /** The view for this control. */
+    @Wire("#button1")
+    private @Nullable Button button1;
+
+    /** The view for that control. */
+    @Wire("#button2")
+    private @Nullable Button button2;
 
     @SuppressWarnings({"i18n"})
     private static @Localized String fakeLocalizer(final Callable<String> s) {
@@ -91,73 +86,58 @@ public final class TestController extends SCRSelectorComposer<Component>
     }
 
     private Caller getCaller() {
-        @Nullable User user = (User) Sessions.getCurrent().getAttribute(Users.USER);
         assert userAdmin != null : "@AssumeAssertion(nullness)";
-
-        return new CallerImpl(userAdmin.getAuthorization(user));
+        return Users.getCaller(userAdmin);
     }
 
     @Override
     public void doAfterCompose(final Component comp) throws Exception {
         super.doAfterCompose(comp);
-        assert label1 != null : "@AssumeAssertion(nullness)";
 
+        // Initialize the buttons' enabled/disabled status
+        assert userAdmin != null : "@AssumeAssertion(nullness)";
+        boolean notManager = !Users.getCaller(userAdmin).authorization().hasRole("manager");
+        assert button1 != null : "@AssumeAssertion(nullness)";
+        button1.setDisabled(notManager);
+        assert button2 != null : "@AssumeAssertion(nullness)";
+        button2.setDisabled(notManager);
+
+        // Initialize the label text
+        assert label1 != null : "@AssumeAssertion(nullness)";
         label1.setValue(fakeLocalizer(() -> {
             assert testService != null : "@AssumeAssertion(nullness)";
             return testService.getValue(getCaller());
         }));
 
-        // ZK events
-        EventQueue eventQueue = EventQueues.lookup(QUEUE, comp.getDesktop().getSession(), true);
-        LOGGER.info("Do after compose, window " + comp
-            + ", desktop " + getSelf().getDesktop()
-            + ", session " + getSelf().getDesktop().getSession()
-            + ", queue " + eventQueue);
-        if (eventQueue == null) {
-            throw new Exception("ZK event queue " + QUEUE + " not found and could not be created");
-        }
-        eventQueue.subscribe(this);
-
-        // OSGi events
-        connect(TestService.EVENT_TOPIC, eventQueue, (Consumer<String>) s -> {
-            LOGGER.info("Updating label " + label1 + " to " + s);
-            assert label1 != null : "@AssumeAssertion(nullness)";
-            label1.setValue(fakeLocalizer(() -> {
-                assert testService != null : "@AssumeAssertion(nullness)";
-                return testService.getValue(getCaller());
-            }));
+        // Reactively maintain the buttons' enabled/disabled status
+        Users.observable().subscribe(user -> {
+            assert userAdmin != null : "@AssumeAssertion(nullness)";
+            boolean notManager2 = !Users.getCaller(userAdmin).authorization().hasRole("manager");
+            assert button1 != null : "@AssumeAssertion(nullness)";
+            button1.setDisabled(notManager2);
+            assert button2 != null : "@AssumeAssertion(nullness)";
+            button2.setDisabled(notManager2);
         });
-    }
 
-    /**
-     * Forwards OSGi EventAdmin traffic as ZK events.
-     *
-     * @param <T>  the type of stream
-     * @param topic  the source, the name of an OSGi EventAdmin queue
-     * @param eventQueue  the channel
-     * @param consumer  the destination
-     */
-    private <T> void connect(final String topic, final EventQueue eventQueue, final Consumer<T> consumer) {
+        // Reactively maintain the label text
+        RxOSGi
+            .fromTopic(TestService.EVENT_TOPIC, getBundleContext())
+            .map(event -> (String) event.getProperty("value"))
+            .subscribe(string -> {
+                Desktop desktop = comp.getDesktop();
+                try {
+                    Executions.activate(desktop);
+                    try {
+                        assert label1 != null : "@AssumeAssertion(nullness)";
+                        label1.setValue(fakeLocalizer(() -> string));
 
-        Hashtable ht = new Hashtable();
-        ht.put(EVENT_TOPIC, new String[] {topic});
-
-        getBundleContext().registerService(EventHandler.class.getName(), new EventHandler() {
-            @Override
-            public void handleEvent(final Event event) {
-                LOGGER.info("Handle OSGi event: " + event);
-
-                switch (event.getTopic()) {
-                case TestService.EVENT_TOPIC:
-                    eventQueue.publish(new ConsumerEvent<T>((T) event.getProperty("value"), consumer));
-                    break;
-
-                default:
-                    LOGGER.warn("Unsupported topic: " + event.getTopic());
-                    break;
+                    } finally {
+                        Executions.deactivate(desktop);
+                    }
+                } catch (DesktopUnavailableException e) {
+                    LOGGER.warn("Desktop not longer exists", e);
                 }
-            }
-        }, ht);
+            });
     }
 
     /**
@@ -168,14 +148,5 @@ public final class TestController extends SCRSelectorComposer<Component>
     public void onClickButton(final MouseEvent mouseEvent) throws CallerNotAuthorizedException {
         assert testService != null : "@AssumeAssertion(nullness)";
         testService.setValue(((Button) mouseEvent.getTarget()).getLabel(), getCaller());
-    }
-
-
-    // Implementation of ZK EventListener<ConsumerEvent<String>>
-
-    @Override
-    public void onEvent(final ConsumerEvent<String> consumerEvent) throws Exception {
-        LOGGER.info("Handle ZK event " + consumerEvent);
-        consumerEvent.run();
     }
 }
