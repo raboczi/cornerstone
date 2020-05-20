68004962edbec6c31382772b21612453db429774
raboczi <raboczi@icloud.com>
2020-05-20 20:39:23 +1000

    Added the source code for the Circadian ZK theme, and made it default.

---
+++ zk/circadian/src/org/zkoss/theme/circadian/Version.java
@@ -0,0 +1,52 @@
+/* Version.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Oct 6, 2010 12:30:37 PM , Created by jimmy
+}}IS_NOTE
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under LGPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.theme.circadian;
+
+/*-
+ * #%L
+ * Cornerstone :: ZK theme :: Circadian
+ * %%
+ * Copyright (C) 2019 - 2020 Simon Raboczi
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ *  The version of the sapphire theme.
+ * {@link #UID} must be the same as the version specified in lang-addon.xml.
+ * @author Jimmy
+ */
+public class Version {
+	/** Returns the version UID.
+	 */
+	public static final String UID = "0.1-SNAPSHOT";
+}
---
+++ zk/circadian/src/org/zkoss/theme/circadian/ucircadianThemeWebAppInit.java
@@ -0,0 +1,66 @@
+/* ucircadianThemeWebAppInit.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Jun 29, 2010 11:56:16 AM , Created by Sam
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under LGPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.theme.circadian;
+
+/*-
+ * #%L
+ * Cornerstone :: ZK theme :: Circadian
+ * %%
+ * Copyright (C) 2019 - 2020 Simon Raboczi
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.WebApps;
+import org.zkoss.zk.ui.util.WebAppInit;
+import org.zkoss.zkmax.theme.ResponsiveThemeRegistry;
+import org.zkoss.zul.theme.Themes;
+
+/**
+ * Initial the theme relative setting, includes
+ * Library property setting, Theme provider setting and Component definition setting 
+ * 
+ */
+public class ucircadianThemeWebAppInit implements WebAppInit {
+
+	private final static String THEME_NAME = "circadian";
+	private final static String THEME_DISPLAY = "Circadian";
+	private final static int THEME_PRIORITY = 700;
+	
+	public void init(WebApp webapp) throws Exception {
+		Themes.register(THEME_NAME, THEME_DISPLAY, THEME_PRIORITY);
+		// Bug ZK-2963: register sapphire theme for tablet responsive theme
+		String edition = WebApps.getEdition();
+		if ("EE".equals(edition)) {
+			Themes.register(ResponsiveThemeRegistry.TABLET_PREFIX + THEME_NAME, THEME_DISPLAY, THEME_PRIORITY);
+		}
+	}
+}
